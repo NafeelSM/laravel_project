a17894cf597a9bb507cb1b123c88b888bb196b72
Nafeel SM <nafeelsm2018@gmail.com>
2024-09-04 21:41:21 +0530

    TeacherController_Create

---
+++ resources/views/teachers/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layout')
+@section('content')
+
+
+<div class="card">
+    <div class="card-header">
+        <h2>Teachers Application</h2>
+    </div>
+    <div class="card-body">
+        <a href="{{ url('/teachers/create') }}" class="btn btn-success btn-sm" title="Add New Teacher">
+            <i class="fa fa-plus" aria-hidden="true"></i> Add New
+        </a>
+        <br />
+        <br />
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Mobile</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($teachers as $item)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->address }}</td>
+                        <td>{{ $item->mobile }}</td>
+
+                        <td>
+                            <a href="{{ url('/teachers/' . $item->id) }}" title="View Teacher"><button class="btn btn-info btn-sm">
+                                <i class="fa fa-eye" aria-hidden="true"></i> View </button></a>
+                            <a href="{{ url('/teachers/' . $item->id . '/edit') }}" title="Edit Teacher"><button class="btn btn-primary btn-sm">
+                                <i class="fa fa-pencil-alt" aria-hidden="true"></i> Edit </button></a>
+                            <form method="POST" action="{{ url('/teachers/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                {{ method_field('DELETE') }}
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Teacher" onclick="return confirm('Confirm delete?')">
+                                    <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+
 
 Route::get('/', function () {
     return view('layout');
 });
 
 Route::resource('/students', StudentController::class);
+
+Route::resource('/teachers', TeacherController::class);
