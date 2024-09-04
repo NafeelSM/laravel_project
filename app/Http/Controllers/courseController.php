80346490507699db26432d82667ceea36f64c41e
Nafeel SM <nafeelsm2018@gmail.com>
2024-09-04 22:14:52 +0530

    courseDatabase-migration

--- app/Http/Controllers/courseController.php
+++ app/Http/Controllers/courseController.php
@@ -3,8 +3,72 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\course;
+use Illuminate\View\View;
 
 class courseController extends Controller
 {
-    //
+    public function index()
+    {
+        $courses = course::all();
+        return view ('courses.index')->with('courses', $courses);
+    }
+
+     /**
+     * Show the form for creating a new resource.
+     */
+    /**Create function */
+    public function create()
+    {
+        return view('courses.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    /**Store function */
+    public function store(Request $request): RedirectResponse
+    {
+        $input = $request->all();
+        course::create($input);
+        return redirect('courses')->with('flash_message', 'Course Addedd!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): View
+    {
+        $courses = course::find($id);
+        return view('courses.show')->with('courses', $courses);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): View
+    {
+        $courses = course::find($id);
+        return view('courses.edit')->with('courses', $courses);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $courses = course::find($id);
+        $input = $request->all();
+        $courses->update($input);
+        return redirect('courses')->with('flash_message', 'Courses Updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        course::destroy($id);
+        return redirect('courses')->with('flash_message', 'Course Deleted!');
+    }
 }
---
+++ resources/views/courses/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+@section('content')
+<div class="card">
+    <div class="card-header">Edit Page</div>
+    <div class="card-body">
+
+        <form action="{{ url('courses/' . $courses->id) }}" method="post">
+            {!! csrf_field() !!}
+            @method("PATCH")
+
+            <input type="hidden" name="id" id="id" value="{{$courses->id}}" id="id" />
+
+            <label>Name</label><br>
+            <input type="text" name="name" id="name" value="{{$courses->name}}" class="form-control"><br>
+
+            <label>Syllebus</label><br>
+            <input type="text" name="syllebus" id="syllebus" value="{{$courses->address}}" class="form-control"><br>
+
+            <label>Duration</label><br>
+            <input type="text" name="duration" id="duration" value="{{$courses->mobile}}" class="form-control"><br>
+
+            <input type="submit" value="Update" class="btn btn-success"><br>
+        </form>
+
+    </div>
+</div>
+@stop
