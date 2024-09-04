b0e729180f54ab34c738d69f26a9f13bb976666d
Nafeel SM <nafeelsm2018@gmail.com>
2024-09-04 21:54:38 +0530

    courseController-created

---
+++ app/Models/course.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class course extends Model
+{
+    protected $table = 'courses';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'syllebus', 'duration'];
+    use HasFactory;
+}
